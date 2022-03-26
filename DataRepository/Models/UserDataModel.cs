77d0fd0c11ca2bf25d61105ebd8169fe167a974b
vandana l <vandana.developer2019@gmail.com>
2022-03-26 22:44:43 +1300

    Login - Push

---
+++ DataRepository/Models/UserDataModel.cs
@@ -0,0 +1,27 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataRepository.Models
+{
+    public class UserDataModel
+    {
+        [DisplayName("Email")]
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+
+        [DisplayName("Confirm Password")]
+        [Write(false)]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ SchoolProjectCoreWeb/Controllers/UserInfoController.cs
@@ -0,0 +1,52 @@
+﻿using DataRepository.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolProjectCoreWeb.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserInfoController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(UserDataModel userDataModel)
+        {
+            var user = new IdentityUser
+            {
+                UserName = userDataModel.EmailAddress,
+                Email = userDataModel.EmailAddress,
+            };
+            var identityResult = await _userManager.CreateAsync(user, userDataModel.Password);
+            if(identityResult.Succeeded)
+            {
+                TempData["Success"] = "User created";
+            }
+            else
+            {
+
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                  
+                }
+                
+            }
+            return View();
+
+        }
+    }
+}
